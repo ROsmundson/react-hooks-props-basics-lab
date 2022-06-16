0068bd28af41e41c85300aadbc8a28fe9e4df64f
Ryan Osmundson <rosmundson@outlook.com>
2022-06-15 19:22:20 -0600

    commit tree 6f6d21ae99134a88a0d6439bf26511c1535ac632

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import user from "../data/user";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
 
+//In the `App` component, pass down data from the `user` object as props to the `Home` component so that it renders correctly.
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color = { user.color } name = { user.name } city = { user.city }/>
+      <About bio = { user.bio } links = { user.links }/>
     </div>
   );
 }
 
 export default App;
+
